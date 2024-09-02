cb8311b04f6518ff835cc6fb813955d463b3685a
unknown <autraurdo@gmail.com>
2024-09-03 01:06:32 +0300

    first commit

---
+++ Debug/Core/Src/l3gd20.d
@@ -0,0 +1,4 @@
+Core/Src/l3gd20.o: ../Core/Src/l3gd20.c ../Core/Inc/l3gd20.h \
+ ../Core/Inc/gyro.h
+../Core/Inc/l3gd20.h:
+../Core/Inc/gyro.h:
---
+++ Debug/Core/Src/lsm303dlhc.d
@@ -0,0 +1,4 @@
+Core/Src/lsm303dlhc.o: ../Core/Src/lsm303dlhc.c ../Core/Inc/lsm303dlhc.h \
+ ../Core/Inc/accelero.h
+../Core/Inc/lsm303dlhc.h:
+../Core/Inc/accelero.h:
